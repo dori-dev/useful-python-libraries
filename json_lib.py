fee9056ab33feb0dd4163e833983533e1dba5451
mohammad <mr.dori.dev@gmail.com>
2022-03-27 13:19:37 +0430

    simple use json

--- json_lib.py
+++ json_lib.py
@@ -1,2 +1,12 @@
 """json library
 """
+# JSON: JavaScript Object Notation
+import json
+
+with open("assets/data.json", "r") as school_json:
+    school_data = json.load(school_json)
+
+print(type(school_data))
+# list
+print(school_data[0])
+# {'name': 'ali', 'age': 18, 'grades': [20, 19, 20, 15.75], 'address': None}
