b4d54afb6a5f5bc4cd65aee6f88f9045950ad970
mohammad <mr.dori.dev@gmail.com>
2022-03-27 13:59:55 +0430

    load and dump class object to json files

--- json_lib.py
+++ json_lib.py
@@ -2,6 +2,7 @@
 """
 # JSON: JavaScript Object Notation
 import json
+from unicodedata import name
 
 
 with open("assets/school_info.json", "r") as json_file:
@@ -40,3 +41,35 @@ with open('info.json', 'w') as json_file:
 with open('info.json', 'r') as json_file:
     info = json.load(json_file)
 print(info)
+
+
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+
+class PersonEncoder(json.JSONEncoder):
+    def default(self, obj: object) -> dict:
+        if isinstance(obj, Person):
+            return {
+                "__person__": True,
+                "name": obj.name,
+                "age": obj.age
+            }
+        return super().default(obj)
+
+
+def person_decoder(obj: dict) -> object:
+    if '__person__' in obj:
+        return Person(obj['name'], obj['age'])
+    return obj
+
+
+p1 = Person('zahra', 19)
+with open('person_data.json', 'w') as json_file:
+    json.dump(p1, json_file, indent=4, cls=PersonEncoder)
+with open('person_data.json', 'r') as json_file:
+    data = json.load(json_file, object_hook=person_decoder)
+print(data)
+print(data.name)
