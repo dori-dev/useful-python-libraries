47bc810b7b19b4dc2a5f513806a252eb9092c6f9
mohammad <mr.dori.dev@gmail.com>
2022-03-27 13:31:02 +0430

    dump data to json file

--- json_lib.py
+++ json_lib.py
@@ -3,10 +3,26 @@
 # JSON: JavaScript Object Notation
 import json
 
-with open("assets/data.json", "r") as school_json:
-    school_data = json.load(school_json)
+with open("assets/school_info.json", "r") as json_school:
+    school_data = json.load(json_school)
 
 print(type(school_data))
 # list
 print(school_data[0])
 # {'name': 'ali', 'age': 18, 'grades': [20, 19, 20, 15.75], 'address': None}
+
+
+list_data = [
+    1, 2, 3.5,
+    'hello', 'wow',
+    True, None, False,
+    [10, 20, 30],
+    {
+        "name": "mohammad",
+        "age": 21,
+    },
+    (1, 2, 3, 4)
+]
+
+with open('data.json', 'w') as json_data:
+    json.dump(list_data, json_data)
